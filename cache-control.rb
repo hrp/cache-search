79624a347018b86540ea31595b0480429f0c2ffc
Hiten Parmar <hrparmar@gmail.com>
2014-03-03 19:28:42 -0800

    Add some structure

--- cache-control.rb
+++ cache-control.rb
@@ -1,35 +1,49 @@
 require 'csv'
 require 'typhoeus'
 
-def top100
-	filename = 'top-100.csv'
-	CSV.read filename
-end
+class CacheChecker
+	def initialize
+		@results = []
+		@hydra = Typhoeus::Hydra.hydra
+	end
 
-# Use Typhoeus for parallel downloads
-hydra = Typhoeus::Hydra.hydra
-results = []
+	def run!
+		check_sites
+		write_csv
+	end
 
-top100.each do |rank, url|
-	req = Typhoeus::Request.new(url, timeout: 10, followlocation: true)
-	hydra.queue(req)
+	def domains(filename = 'top-100.csv')
+		CSV.read filename
+	end
 
-	req.on_complete do |response|
-		if response.timed_out?
-			msg = "TIMED OUT"
-		else
-			msg = response.headers["Cache-Control"]
-			puts "#{url} - #{msg}"
+
+	def check_sites
+		domains.each do |rank, url|
+			req = Typhoeus::Request.new(url, timeout: 10, followlocation: true)
+			@hydra.queue(req)
+
+			req.on_complete do |response|
+				if response.timed_out?
+					msg = "TIMED OUT"
+				else
+					msg = response.headers["Cache-Control"]
+					puts "#{url} - #{msg}"
+				end
+				@results << [rank, url, msg]
+			end
 		end
-		results << [rank, url, msg]
+
+		@hydra.run
+	end
+
+  def write_csv(filename = 'top-100-cache.csv')
+		CSV.open(filename, 'w') do |output|
+			@results.sort_by!{|a| a.first.to_i}.each do |res|
+				output << res
+			end
+		end
 	end
 end
 
-hydra.run
-
-# Write results to a csv file
-CSV.open('top-100-cache.csv', 'w') do |output|
-	results.sort_by!{|a| a.first.to_i}.each do |res|
-		output << res
-	end
-end
+c = CacheChecker.new
+c.run!
